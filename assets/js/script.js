96dbc811205754303e358cf8aa4f55f2ac265a3d
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-11-23 20:59:30 -0500

    updated movie info section to populate data from API including image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var disneyApi = 'https://api.disneyapi.dev/characters';
-var reviewApi = 'https://api.themoviedb.org/3/movie/550?api_key=091a5c8f390a977d67ab12f38ec85102';
+var reviewApi = 'https://api.themoviedb.org/3/movie/812?api_key=091a5c8f390a977d67ab12f38ec85102';
 var characterFilmSection = document.getElementById("character-films");
+var movieName = document.querySelector(".movie-name")
+var movieInfo = document.querySelector(".movie-info")
+var movieImage = "https://image.tmdb.org/t/p/w500/"
+var textInput = document.querySelector('.text-input');
 var characterlist = [];
 var movielist = [];
-var textInput = document.querySelector('.text-input');
 
 function evaluateInput(event) {
     characterFilmSection.textContent = "";
@@ -33,7 +36,6 @@ function evaluateInput(event) {
 
 //append text to character selection section in index
             characterFilmSection.append(characterFilm);
-            console.log(characterFilm);
          }
         })
 }
@@ -44,6 +46,34 @@ function getReviewApi() {
         console.log(response);
         response.json().then(function (data) {
             console.log(data);
+// fetch movie title and append to index
+            var movieTitle = document.createElement("p");
+            movieTitle.classList.add("title")
+            movieTitle.textContent = data.title
+            movieName.append(movieTitle);
+
+// fetch movie info and append to index
+var imgPull = document.createElement("img");
+    imgPull.setAttribute("src", movieImage + data.backdrop_path);
+    movieInfo.append(imgPull);
+
+// fetch movie info and append to index
+var movieInfoPull = document.createElement("p");
+movieInfoPull.setAttribute("style", "font-size: 14px")
+    movieInfoPull.textContent = data.release_date + " | " + data.runtime + " min" + " | " + data.genres[0].name
+    movieInfo.append(movieInfoPull);
+        
+// fetch movie rating and append to index
+var movieInfoPull = document.createElement("p");
+    movieInfoPull.textContent = "Rating: " + data.vote_average
+    movieInfo.append(movieInfoPull);
+
+// fetch movie overview and append to index
+var movieInfoPull = document.createElement("p");
+    movieInfoPull.textContent = data.overview
+    movieInfoPull.setAttribute("style", "font-size: 14px")
+    movieInfo.append(movieInfoPull);
+
         })
     })
 }
