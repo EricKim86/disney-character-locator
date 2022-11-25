287f64817503de7b84bb7281083c9c15e5a75386
LayneNi <laynenigro@gmail.com>
2022-11-25 11:25:37 -0500

    added button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,62 @@ var characterFilmSection = document.getElementById("character-films");
 var characterlist = [];
 var movielist = [];
 var textInput = document.querySelector('.text-input');
+var searchHistoryBtnEl = document.querySelector('#search-history-buttons');
+
+
+// addign data to local storage
+function saveToStorage(value) {
+    var searchHistoryArray = JSON.parse(localStorage.getItem('characters')) || []
+    if (searchHistoryArray.includes(value)) {
+        return
+    }
+    searchHistoryArray.push(value);
+    localStorage.setItem('characters', JSON.stringify(searchHistoryArray))
+}
+
+function renderStorage() {
+    var searchHistoryArray = JSON.parse(localStorage.getItem('characters')) || []
+    if (searchHistoryArray.length === 0) {
+        return
+    }
+    // searchHistoryBtnEl.innerHTML = ""
+    console.log("Rendering storage")
+    for (let i = 0; i < searchHistoryArray.length; i++) {
+        console.log(searchHistoryArray[i])
+        // var searchHistoryBtnEl = document.querySelector('#search-history-buttons');
+        var searchHistoryBtn = document.createElement("button");
+        searchHistoryBtn.textContent = searchHistoryArray[i]
+        searchHistoryBtnEl.appendChild(searchHistoryBtn)
+    }
+}
+
+searchHistoryBtnEl.addEventListener("click", function (event) {
+    console.log("Good");
+    var searchHistoryBtnValue = (event.target.textContent)
+    console.log(searchHistoryBtnValue);
+    var searchHistoryFetch = 'https://api.disneyapi.dev/character?name=' + searchHistoryBtnValue;
+    fetch(searchHistoryFetch)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            // loop based on # of films for selected character
+            for (var i = 0; i < data.data[0].films.length; i++) {
+
+                //create element and populate film(s)
+                var characterFilm = document.createElement("h3");
+                characterFilm.textContent = data.data[0].films[i]
+
+                //append text to character selection section in index
+                characterFilmSection.append(characterFilm);
+
+            }
+        })
+}
+
+);
 
 function evaluateInput(event) {
     event.preventDefault()
@@ -11,38 +67,41 @@ function evaluateInput(event) {
     var characterInput = document.getElementById('search-text');
     var characterVal = characterInput.value;
     var characterFetch = 'https://api.disneyapi.dev/character?name=' + characterVal;
+    saveToStorage(characterVal)
 
     console.log(characterVal);
-//fetch disney api
+    //fetch disney api
     fetch(characterFetch)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-// loop based on # of films for selected character
-         for (var i = 0; i < data.data[0].films.length; i++) {
+            // loop based on # of films for selected character
+            for (var i = 0; i < data.data[0].films.length; i++) {
 
-//create element and populate film(s)
-            var characterFilm = document.createElement("h3");
-            characterFilm.textContent = data.data[0].films[i]
+                //create element and populate film(s)
+                var characterFilm = document.createElement("h3");
+                characterFilm.textContent = data.data[0].films[i]
 
-//append text to character selection section in index
-            characterFilmSection.append(characterFilm);
-         }
+                //append text to character selection section in index
+                characterFilmSection.append(characterFilm);
+
+            }
         })
 }
 
 function getReviewApi() {
     fetch(reviewApi)
-    .then(function(response) {
-        console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
+        .then(function (response) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+            })
         })
-    })
 }
 textInput.addEventListener('submit', evaluateInput);
+textInput.addEventListener('submit', renderStorage);
 
 getReviewApi();
